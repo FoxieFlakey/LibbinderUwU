932afe9e9a5385709ef6091b61b482d99f1ce613
Foxie Flakey <foxieflakey@gmail.com>
2025-12-04 13:51:44 +0700

    Ability to turn built packet back to packet builder

--- libbinder/src/packet.rs
+++ libbinder/src/packet.rs
@@ -3,7 +3,7 @@ use std::os::fd::BorrowedFd;
 use enumflags2::BitFlags;
 use libbinder_raw::{ObjectRef, Transaction, TransactionFlag};
 
-use crate::command_buffer::{Command, CommandBuffer};
+use crate::{command_buffer::{Command, CommandBuffer}, packet::builder::PacketBuilder};
 
 pub mod builder;
 
@@ -23,10 +23,36 @@ pub struct Packet<'binder> {
   // be dropped
   transaction: Transaction<'binder, 'static, 'static>,
   
-  #[expect(unused)]
   data_buffer: Vec<u8>,
-  #[expect(unused)]
   offset_buffer: Vec<usize>
+}
+
+impl<'binder> Into<PacketBuilder<'binder>> for Packet<'binder> {
+  fn into(mut self) -> PacketBuilder<'binder> {
+    let common = self.transaction.get_common();
+    match &self.transaction {
+      Transaction::KernelManaged(x) => {
+        // The buffer associated with it is kernel managed
+        // we have to copy...
+        
+        self.data_buffer.copy_from_slice(x.get_data().data_slice);
+        self.offset_buffer.copy_from_slice(x.get_data().offsets);
+      }
+      
+      Transaction::NotKernelManaged(_x) => {
+        // The buffer associated with it, is managed by Packet, so nothing need
+        // to be done
+      }
+    };
+    
+    PacketBuilder {
+      binder_dev: Some(self.binder_dev),
+      code: Some(common.code),
+      data_buffer: self.data_buffer,
+      offsets_buffer: self.offset_buffer,
+      flags: Some(common.flags)
+    }
+  }
 }
 
 impl<'binder> Packet<'binder> {
