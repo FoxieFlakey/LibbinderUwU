5c5815008cfaad8a563198312ecc25105b59b1be
Foxie Flakey <foxieflakey@gmail.com>
2025-12-06 11:52:24 +0700

    Handle the transactions :3

--- binder-test/src/service_manager/mod.rs
+++ binder-test/src/service_manager/mod.rs
@@ -1,9 +1,9 @@
 use std::sync::Arc;
 
-use libbinder::{formats::dead_simple::DeadSimpleFormat, packet::{Packet, builder::PacketBuilder}};
+use libbinder::{formats::dead_simple::{DeadSimpleFormat, DeadSimpleFormatReader}, packet::{Packet, builder::PacketBuilder}};
 use libbinder_runtime::{Runtime, binder_object::BinderObject};
 
-use crate::{common::log, interface::IServiceManager};
+use crate::{common::log, interface::{ISERVICE_MANAGER_CODE_BWAH_UWU, ISERVICE_MANAGER_CODE_LENGTH_OF_STRING, ISERVICE_MANAGER_RET_ERR, ISERVICE_MANAGER_RET_OK, IServiceManager}};
 
 struct ContextManager;
 
@@ -20,11 +20,35 @@ impl IServiceManager for ContextManager {
 
 impl BinderObject<ContextManager> for ContextManager {
   fn on_packet(&self, _runtime: &Arc<Runtime<ContextManager>>, packet: &Packet<'_>, reply_builder: &mut PacketBuilder) {
+    reply_builder.set_code(ISERVICE_MANAGER_RET_OK);
     match packet.get_code() {
+      ISERVICE_MANAGER_CODE_LENGTH_OF_STRING => {
+        let mut reader = packet.reader(DeadSimpleFormatReader::new());
+        let Ok(data) = reader.read_str() else {
+          reply_builder.set_code(ISERVICE_MANAGER_RET_ERR)
+            .writer(DeadSimpleFormat::new())
+            .write_str("invalid string");
+          return;
+        };
+        
+        let len = self.length_of_string(data);
+        reply_builder.writer(DeadSimpleFormat::new())
+          .write_usize(len);
+      }
+      ISERVICE_MANAGER_CODE_BWAH_UWU => {
+        let mut reader = packet.reader(DeadSimpleFormatReader::new());
+        let Ok(data) = reader.read_str() else {
+          reply_builder.set_code(ISERVICE_MANAGER_RET_ERR)
+            .writer(DeadSimpleFormat::new())
+            .write_str("invalid string");
+          return;
+        };
+        self.bwah_uwu(data);
+      }
       _ => {
         log!("Received unknown transaction code: {}", packet.get_code());
-        reply_builder.set_code(0x01);
-        reply_builder.writer(DeadSimpleFormat::new())
+        reply_builder.set_code(ISERVICE_MANAGER_RET_ERR)
+          .writer(DeadSimpleFormat::new())
           .write_str("unknown transaction code");
       }
     }
