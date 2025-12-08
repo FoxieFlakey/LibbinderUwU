30a1046ef0f5cbd4439ec1dea1683f3f83a21c96
Foxie Flakey <foxieflakey@gmail.com>
2025-12-08 11:27:24 +0700

    Add header type

--- libbinder-raw/src/object/mod.rs
+++ libbinder-raw/src/object/mod.rs
@@ -1,4 +1,5 @@
 use bytemuck::{Pod, Zeroable};
+use bytemuck_utils::PodData;
 
 const TYPE_LARGE: u8 = 0x85;
 
@@ -10,19 +11,44 @@ const fn pack_chars(c1: u8, c2: u8, c3: u8, c4: u8) -> u32 {
 }
 
 pub(crate)  const BINDER: u32 = pack_chars(b's', b'b', b'*', TYPE_LARGE);
-#[expect(unused)]
 pub(crate)  const WEAK_BINDER: u32 = pack_chars(b'w', b'b', b'*', TYPE_LARGE);
 pub(crate)  const HANDLE: u32 = pack_chars(b's', b'h', b'*', TYPE_LARGE);
-#[expect(unused)]
 pub(crate)  const WEAK_HANDLE: u32 = pack_chars(b'w', b'h', b'*', TYPE_LARGE);
-#[expect(unused)]
 pub(crate)  const FD: u32 = pack_chars(b'f', b'd', b'*', TYPE_LARGE);
-#[expect(unused)]
 pub(crate)  const FDA: u32 = pack_chars(b'f', b'd', b'a', TYPE_LARGE);
-#[expect(unused)]
 pub(crate)  const PTR: u32 = pack_chars(b'p', b't', b'*', TYPE_LARGE);
 
 pub mod reference;
+
+pub enum Type {
+  RemoteReference,
+  LocalReference,
+  WeakRemoteReference,
+  WeakLocalReference,
+  FileDescriptor,
+  FileDescriptorArray,
+  ByteBuffer
+}
+
+impl Type {
+  pub fn bytes_needed() -> usize {
+    size_of::<ObjectHeaderRaw>()
+  }
+  
+  pub fn try_from_bytes(bytes: &[u8]) -> Result<Type, ()> {
+    let raw = PodData::<ObjectHeaderRaw>::from_bytes(bytes);
+    match raw.kind {
+      BINDER => Ok(Type::LocalReference),
+      HANDLE => Ok(Type::RemoteReference),
+      WEAK_BINDER => Ok(Type::WeakLocalReference),
+      WEAK_HANDLE => Ok(Type::WeakRemoteReference),
+      FD => Ok(Type::FileDescriptor),
+      FDA => Ok(Type::FileDescriptorArray),
+      PTR => Ok(Type::ByteBuffer),
+      _ => Err(())
+    }
+  }
+}
 
 // Equivalent to struct binder_object_header
 #[repr(C)]
