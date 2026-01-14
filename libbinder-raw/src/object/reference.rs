9ac56e4f3050a064c091af1b13d7ebf9057890ce
Foxie Flakey <foxieflakey@gmail.com>
2026-01-14 12:24:52 +0700

    Copy too

--- libbinder-raw/src/object/reference.rs
+++ libbinder-raw/src/object/reference.rs
@@ -76,14 +76,14 @@ impl ObjectRef {
   }
 }
 
-#[derive(Clone)]
+#[derive(Copy, Clone)]
 pub struct ObjectRefLocal {
   // Whatever data can be in these, kernel won't touch it
   pub data: usize,
   pub extra_data: usize
 }
 
-#[derive(Clone)]
+#[derive(Copy, Clone)]
 pub struct ObjectRefRemote {
   pub data_handle: u32
 }
